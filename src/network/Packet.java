8216bd4f0d6f6a6cc3489bb6737689058b1abed3
jjkramok@gmail.com <Appelsap7>
2015-04-07 11:49:36 +0200

    Initial commit Protocol.java + added multicast address and port

--- src/network/Packet.java
+++ src/network/Packet.java
@@ -8,7 +8,7 @@ package network;
 public class Packet {
     // -----<=>-----< Fields >-----<=>----- \\
     private byte[] dt;
-
+    private String src; //Not used yet
 
     // -----<=>-----< Constructors >-----<=>----- \\
 
@@ -20,7 +20,7 @@ public class Packet {
 
     // -----<=>-----< Methods >-----<=>----- \\
     /**
-     * Prints the data in a packet to the standart out (as a String)
+     * Prints the data in a packet to the standard out (as a String)
      */
     public void print() {
         System.out.println(new String(this.getData()));
@@ -29,10 +29,18 @@ public class Packet {
 
     // -----<=>-----< Queries >-----<=>----- \\
     /**
-     * Getter for the data field (byte[]) of a packet
+     * Getter for the data field (byte[]) of this packet
      * @return byte[] with the data of the packet
      */
     public byte[] getData() {
         return this.dt;
+    }
+
+    /**
+     * Getter for the source field (String) of this packet
+     * @return String the source address of this packet
+     */
+    public String getSource() {
+        return this.src;
     }
 }
