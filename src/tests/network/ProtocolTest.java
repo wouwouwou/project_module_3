1dcf2f6e45201a5d1258c8a26798fcafe4614bca
wouwouwou <wouter_bos96@hotmail.com>
2015-04-08 23:43:01 +0200

    Stub Implementation Tests Finished

---
+++ src/tests/network/ProtocolTest.java
@@ -0,0 +1,22 @@
+package tests.network;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wouwouwou on 8-4-15.
+ */
+public class ProtocolTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGroupAddress() throws Exception {
+
+    }
+}
