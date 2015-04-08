01a4598ea1e667cd967388ad55e7d9d6a45e6ae4
Tristan de Boer <tristandb1@gmail.com>
2015-04-08 23:01:07 +0200

    Removed some lines that weren't used

---
+++ src/file/FileHandlerTest.java
@@ -0,0 +1,16 @@
+package file;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by tristan on 8-4-15.
+ */
+public class FileHandlerTest {
+    public static void main(String[] args){
+        Path path = Paths.get("/home/tristan/Documents/1.png");
+        FileHandler fileHandler = new FileHandler();
+        fileHandler.sendFile(path, 1, 1);
+
+    }
+}
