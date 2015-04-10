84de441d3b5b8289e801118922d1441b670b0f18
jjkramok@gmail.com <Appelsap7>
2015-04-10 10:02:00 +0200

    Added a custom DestinationNotInTableException

--- src/exceptions/network/DestinationNotInTableException.java
+++ src/exceptions/network/DestinationNotInTableException.java
@@ -1,6 +1,7 @@
 package exceptions.network;
 
 import exceptions.NetworkException;
+import network.Protocol;
 
 /**
  * @author Tim Hintzbergen
@@ -8,4 +9,17 @@ import exceptions.NetworkException;
  */
 public class DestinationNotInTableException extends NetworkException {
 
+    private int destination = -1;
+
+    public DestinationNotInTableException(byte destination) {
+        this.destination = Protocol.fixSign(destination);
+    }
+
+    @Override
+    public String getMessage() {
+        if (this.destination != -1)
+            return "Destination: " + this.destination + " is not found in the routingTable.";
+        else
+            return "the destination is not found in the routingTable.";
+    }
 }
