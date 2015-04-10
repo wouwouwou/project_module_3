0dbcb527cefe9d4e0b9b1703c6b1ddc254fdda12
gerben <gerry2211@gmail.com>
2015-04-10 10:04:14 +0200

    Did a fix to DVR

--- src/network/packethandler/IncomingPacketHandler.java
+++ src/network/packethandler/IncomingPacketHandler.java
@@ -165,7 +165,7 @@ public class IncomingPacketHandler extends PacketHandler {
             for(int i = Protocol.DISCOVERY_HEADER_LENGTH; i < Protocol.DISCOVERY_HEADER_LENGTH + length; i+=3){
 
                 //if the cost if the new entry is lower, use it and forward it
-                if(networkManager.getTableEntryByDestination(packet[i]) == null || packet[i+1] > networkManager.getTableEntryByDestination(packet[i])[1] + 1) {
+                if(networkManager.getTableEntryByDestination(packet[i]) == null || packet[i+1]  + 1 < networkManager.getTableEntryByDestination(packet[i])[1]) {
                     networkManager.addTableEntry(new byte[]{packet[i], (byte) (packet[i + 1] + 1), packet[i + 2]});
                     forward = true;
                 }
