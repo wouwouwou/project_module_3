eb6c30896d279d8dffd6b4d9a96ca6098f895427
jjkramok@gmail.com <Appelsap7>
2015-04-10 14:46:11 +0200

    Even more polish

--- src/exceptions/network/ClientIdNotAvailableException.java
+++ src/exceptions/network/ClientIdNotAvailableException.java
@@ -12,7 +12,7 @@ import network.NetworkManager;
  *     When this Exception is caught it is advised to {@link System#exit(int status) kill the task}.
  *     Exit code -1 should be used.
  * </p>
- * @see NetworkManager#getClientId() getClientId()
+ * @see {@link NetworkManager#getClientId() getClientId()}
  */
 public class ClientIdNotAvailableException extends NetworkException {
 
