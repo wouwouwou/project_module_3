6ef0206375a2489604277425ba88a801b3cbcdb8
jjkramok@gmail.com <Appelsap7>
2015-04-10 09:53:31 +0200

    Added a custom DestinationNotInTableException

---
+++ src/exceptions/network/DestinationNotInTableException.java
@@ -0,0 +1,11 @@
+package exceptions.network;
+
+import exceptions.NetworkException;
+
+/**
+ * @author Tim Hintzbergen
+ * @since 10-4-15.
+ */
+public class DestinationNotInTableException extends NetworkException {
+
+}
