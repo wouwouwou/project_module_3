88ed766115a6db542392808ddee48e4ffdd826b7
jjkramok@gmail.com <Appelsap7>
2015-04-10 15:00:29 +0200

    Added more javaDoc to all custom exception classes

--- src/exceptions/network/InvalidPacketException.java
+++ src/exceptions/network/InvalidPacketException.java
@@ -5,6 +5,7 @@ import exceptions.NetworkException;
 /**
  * @author Wouter Bos
  * @since 8-4-15
+ * A general exception for exceptional behaviour with construction of packets
  */
 public class InvalidPacketException extends NetworkException {
 
--- src/exceptions/network/packet/NullPacketException.java
+++ src/exceptions/network/packet/NullPacketException.java
@@ -5,6 +5,11 @@ import exceptions.network.InvalidPacketException;
 /**
  * @author Wouter Bos
  * @since 8-4-15
+ * A null packet is a packet that is empty
+ * <p>
+ *     A Null packet is a packet with Packet.type = Protocol.NULL_PACKET
+ * </p>
+ * @see //TODO reference to our protocol implementation
  */
 public class NullPacketException extends InvalidPacketException {
 
