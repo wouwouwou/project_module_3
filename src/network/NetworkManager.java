26bc614ff79adc35d52c3b98b02a9f33ef64bcb5
jjkramok@gmail.com <Appelsap7>
2015-04-13 12:05:17 +0200

    Removed an redundant todo

--- src/network/NetworkManager.java
+++ src/network/NetworkManager.java
@@ -360,7 +360,7 @@ public class NetworkManager {
     public void increasePingRound() {
         for(Byte key: connectedClients.keySet()){
             connectedClients.put(key, (byte) (connectedClients.get(key) + 1));
-            if(connectedClients.get(key) > Protocol.MAX_MISSED_PINGROUNDS){ //TODO possible discrepancies with IPH handleDiscovery() / Tim
+            if(connectedClients.get(key) > Protocol.MAX_MISSED_PINGROUNDS){
                 dropTable();
                 setDiscoverySequenceNum((short) (discoverySequenceNum + 1));
                 sendTable();
